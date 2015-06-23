f64c96e2426722ad8ea2f731817324ae34283a87
Chris Emerson <chris@cemerson.co.uk>
2015-06-23 18:55:23 +0100

    Game implementation so far

--- spec/TaxmanGameSpec.php
+++ spec/TaxmanGameSpec.php
@@ -1,6 +1,7 @@
 <?php
 namespace spec\CEmerson\TaxmanGame;
 
+use CEmerson\TaxmanGame\TaxmanGame;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -8,6 +9,47 @@ class TaxmanGameSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('CEmerson\TaxmanGame\TaxmanGame');
+        $this->beConstructedWith(1);
+        $this->shouldHaveType(TaxmanGame::class);
+    }
+
+    function it_is_able_to_start_a_game()
+    {
+        $this->beConstructedWith(20);
+
+        $this->getNumbers()->shouldHaveCount(20);
+    }
+
+    function it_should_mark_1_as_unavailable_at_the_start_of_a_game()
+    {
+        $this->beConstructedWith(5);
+
+        $this->getNumberState(1)->shouldBe(TaxmanGame::STATE_UNAVAILABLE);
+    }
+
+    function it_should_return_available_plays_when_asked_at_the_start_of_a_game()
+    {
+        $this->beConstructedWith(5);
+        $this->getAvailablePlays()->shouldReturn([2, 3, 4, 5]);
+    }
+
+    function it_should_mark_a_number_as_mine_when_i_play_it()
+    {
+        $this->beConstructedWith(5);
+        $this->play(4);
+
+        $this->getNumberState(4)->shouldBe(TaxmanGame::STATE_MINE);
+    }
+
+    function it_should_mark_all_available_factors_as_taxmans_when_i_play_a_number()
+    {
+        $this->beConstructedWith(15);
+        $this->play(12);
+
+        $this->getNumberState(6)->shouldBe(TaxmanGame::STATE_TAXMANS);
+        $this->getNumberState(4)->shouldBe(TaxmanGame::STATE_TAXMANS);
+        $this->getNumberState(3)->shouldBe(TaxmanGame::STATE_TAXMANS);
+        $this->getNumberState(2)->shouldBe(TaxmanGame::STATE_TAXMANS);
+        $this->getNumberState(1)->shouldBe(TaxmanGame::STATE_TAXMANS);
     }
 }
--- src/TaxmanGame.php
+++ src/TaxmanGame.php
@@ -3,4 +3,78 @@ namespace CEmerson\TaxmanGame;
 
 class TaxmanGame
 {
+    private $numbers = [];
+
+    const STATE_AVAILABLE = 0;
+    const STATE_UNAVAILABLE = 1;
+    const STATE_MINE = 2;
+    const STATE_TAXMANS = 3;
+
+    public function __construct($numbers)
+    {
+        $this->numbers = array_fill(1, $numbers, self::STATE_AVAILABLE);
+        $this->numbers[1] = self::STATE_UNAVAILABLE;
+    }
+
+    public function getNumbers()
+    {
+        return $this->numbers;
+    }
+
+    public function getNumberState($number)
+    {
+        return $this->numbers[$number];
+    }
+
+    public function getAvailablePlays()
+    {
+        return array_keys(
+            array_filter($this->numbers, function ($element) {
+                return $element == self::STATE_AVAILABLE;
+            })
+        );
+    }
+
+    public function play($number)
+    {
+        $this->claimNumberAsMine($number);
+        $this->markFactorsAsTaxmans($number);
+    }
+
+    private function markFactorsAsTaxmans($chosenNumber)
+    {
+        foreach (range(1, $chosenNumber) as $number) {
+            if (
+                $this->isFactorOfChosenNumber($number, $chosenNumber)
+                && !$this->numberIsMine($number)
+            ) {
+                $this->claimNumberAsTaxmans($number);
+            }
+        }
+    }
+
+    private function isFactorOfChosenNumber($candidateNumber, $chosenNumber)
+    {
+        return $chosenNumber % $candidateNumber == 0;
+    }
+
+    private function numberIsAvailable($candidateNumber)
+    {
+        return $this->getNumberState($candidateNumber) == self::STATE_AVAILABLE;
+    }
+
+    private function numberIsMine($candidateNumber)
+    {
+        return $this->numbers[$candidateNumber] == self::STATE_MINE;
+    }
+
+    private function claimNumberAsMine($number)
+    {
+        return $this->numbers[$number] = self::STATE_MINE;
+    }
+
+    private function claimNumberAsTaxmans($number)
+    {
+        return $this->numbers[$number] = self::STATE_TAXMANS;
+    }
 }
