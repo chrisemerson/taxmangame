0da1585e94adbd9b8b0b4df64f1919177091885e
Chris Emerson <chris@cemerson.co.uk>
2015-06-24 18:40:53 +0100

    Change script to output more stats and to take to/from arguments on the command line

--- taxman.php
+++ taxman.php
@@ -1,26 +1,47 @@
 <?php
+ini_set('memory_limit', '2048M');
+
 require_once __DIR__ . "/vendor/autoload.php";
 
 use CEmerson\TaxmanGame\TaxmanGame;
 
-playGame(intval($argv[1]));
+foreach (range(intval($argv[1]), intval($argv[2])) as $number) {
+    playGame($number);
+}
 
 function playGame($number)
 {
+    $startTime = time();
+
     $game = new TaxmanGame($number);
     $scores = playAllAvailablePlays($game);
 
     $maxScore = 0;
-    $maxIndex = 0;
+    $taxmanScoreAtMax = 0;
+    $maxIndexes = [];
 
-    foreach ($scores as $index => $score) {
-        if ($score['score'] > $maxScore) {
-            $maxScore = $score['score'];
-            $maxIndex = $index;
+    foreach ($scores as $score) {
+        if ($score['score'][0] > $maxScore) {
+            $maxScore = $score['score'][0];
+            $taxmanScoreAtMax = $score['score'][1];
         }
     }
 
-    echo "Best possible score with " . $number . " numbers is " . $maxScore . " with sequence " . implode(" ", $scores[$maxIndex]['plays']) . PHP_EOL;
+    foreach ($scores as $index => $score) {
+        if ($score['score'][0] == $maxScore) {
+            $maxIndexes[] = $index;
+        }
+    }
+
+    $endTime = time();
+
+    echo "Best possible score with " . $number . " numbers is " . $maxScore . "-" . $taxmanScoreAtMax . " with " . count($maxIndexes) . " different sequences:" . PHP_EOL;
+
+    foreach ($maxIndexes as $maxIndex) {
+        echo implode(" ", $scores[$maxIndex]['plays']) . PHP_EOL;
+    }
+
+    echo "Played " . count($scores) . " games and took " . ($endTime - $startTime) . " seconds" . PHP_EOL . PHP_EOL;
 }
 
 function playAllAvailablePlays(TaxmanGame $game, $scores = [])
@@ -29,7 +50,7 @@ function playAllAvailablePlays(TaxmanGame $game, $scores = [])
 
     if (count($availablePlays) == 0) {
         $scores[] = [
-            'score' => $game->getScores()[0],
+            'score' => $game->getScores(),
             'plays' => $game->getPlaySequence()
         ];
     } else {
